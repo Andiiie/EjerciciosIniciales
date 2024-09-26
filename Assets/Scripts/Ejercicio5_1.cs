168696a390117392ae214be1861a210f6cb48598
Andiiie <andreaalsina0@gmail.com>
2024-09-26 21:18:20 +0200

    26.09

---
+++ Assets/Scripts/Ejercicio5_1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio5_1 : MonoBehaviour
+{
+    [SerializeField] int Player1;
+    [SerializeField] int Player2;
+    int vidasTotales;
+
+
+    void Start()
+    {
+       Player1 = 5;
+       Player2 = 6;
+
+
+    }
+}
